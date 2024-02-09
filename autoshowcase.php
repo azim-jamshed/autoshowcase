2b6ec5aaa91991e1b0ae0a479f4eaa7da1b407f7
azim-jamshed <sadmanazim.bd@gmail.com>
2024-02-09 19:15:41 +0600

    hello

---
+++ autoshowcase.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Plugin Name:      AutoShowcase
+ * Plugin URI:        https://azimjamshed.com/autoshowcase/
+ * Description:       AutoShowcase: Your Cars, Your Way | Car Listing.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Azim Jamshed
+ * Author URI:        https://azimjamshed.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://azimjamshed.com/autoshowcase/
+ * Text Domain:       autoshowcase
+ * Domain Path:       /languages
+ */
+
+
+echo "Hello";
+
+
